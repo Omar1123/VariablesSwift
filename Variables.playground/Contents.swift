0305f2f94aae76c524d96c4e3b08c33974958389
Omar <Omar@Mac-mini-de-Omar.local>
2017-05-23 15:25:56 -0600

    ADD: Vars

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func calculateArea(length: Int, width: Int) -> Int {
+    let area = length * width;
+    return area;
+}
+
+let shape1Area = calculateArea(length: 5, width: 4);
+let shape2Area = calculateArea(length: 8, width: 6);
+let shape3Area = calculateArea(length: 1, width: 6);
+
