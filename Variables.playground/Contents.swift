bdd2adebcb5debb792dcaaf58da83ff86a1149d8
Omar <Omar@Mac-mini-de-Omar.local>
2017-05-23 15:34:51 -0600

    ADD: Swift numbers

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -13,3 +13,16 @@ let shape1Area = calculateArea(length: 5, width: 4);
 let shape2Area = calculateArea(length: 8, width: 6);
 let shape3Area = calculateArea(length: 1, width: 6);
 
+var bankAccountBalance = 500.00
+var sigourneWeaverAlienStomperShoes = 350.00
+
+func pucharseItem(currentBalance: Double, itemPrice: Double) -> Double {
+    if itemPrice <= currentBalance {
+        print("Purcharsed item for: $\(itemPrice)")
+        return currentBalance - itemPrice
+    } else {
+        return currentBalance
+    }
+}
+
+
